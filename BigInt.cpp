e20ae3d73082177031d452f06c6ead0786c71457
revanthreddyk66 <revanthreddyk.66@gmail.com>
2025-11-28 19:04:29 +0530

    Update BigInt.cpp

--- BigInt.cpp
+++ BigInt.cpp
@@ -1,189 +1,163 @@
-#include "BigInt.h"
+// BigInt.h (single-file presentation)
+#include <bits/stdc++.h>
+using namespace std;
 
-// --- Constructors ---
+class BigInt {
+public:
+    string number;      // decimal digits, no leading zeros except "0"
+    bool is_negative;
+
+    // --- constructors
+    BigInt();
+    BigInt(long long n);
+    BigInt(const string& s);
+
+    // --- arithmetic operators
+    BigInt operator+(const BigInt& other) const;
+    BigInt operator-(const BigInt& other) const;
+    BigInt operator*(const BigInt& other) const;
+
+    // --- comparisons
+    bool operator==(const BigInt& other) const;
+    bool operator!=(const BigInt& other) const;
+    bool operator<(const BigInt& other) const;
+    bool operator>(const BigInt& other) const;
+    bool operator<=(const BigInt& other) const;
+    bool operator>=(const BigInt& other) const;
+
+    // --- helpers
+private:
+    string add(const string& a, const string& b) const;
+    string subtract(const string& a, const string& b) const; // assumes a >= b
+    string multiply(const string& a, const string& b) const;
+    static string strip_leading_zeros(const string& s);
+    // friend IO
+    friend ostream& operator<<(ostream& os, const BigInt& bi);
+};
+
+// --- implementations (concise)
 BigInt::BigInt() : number("0"), is_negative(false) {}
-
 BigInt::BigInt(long long n) {
-    if (n < 0) {
-        is_negative = true;
-        n = -n;
-    } else {
-        is_negative = false;
-    }
+    if (n < 0) { is_negative = true; n = -n; } else is_negative = false;
     number = to_string(n);
 }
-
 BigInt::BigInt(const string& s) {
-    if (s.empty() || (s.length() == 1 && s[0] == '0')) {
-        number = "0";
-        is_negative = false;
-    } else if (s[0] == '-') {
-        number = s.substr(1);
-        is_negative = true;
-    } else {
-        number = s;
-        is_negative = false;
-    }
+    if (s.empty()) { number = "0"; is_negative = false; return; }
+    if (s[0] == '-') { is_negative = true; number = s.substr(1); }
+    else { is_negative = false; number = s; }
+    number = strip_leading_zeros(number);
+    if (number == "0") is_negative = false;
 }
 
-// --- Helper Functions ---
-string BigInt::add(string num1, string num2) const {
-    string result = "";
-    int i = num1.length() - 1;
-    int j = num2.length() - 1;
-    int carry = 0;
-
-    while (i >= 0 || j >= 0 || carry) {
-        int sum = carry;
-        if (i >= 0) sum += num1[i--] - '0';
-        if (j >= 0) sum += num2[j--] - '0';
-        result += to_string(sum % 10);
-        carry = sum / 10;
-    }
-    reverse(result.begin(), result.end());
-    return result;
+string BigInt::strip_leading_zeros(const string& s) {
+    size_t p = s.find_first_not_of('0');
+    if (p == string::npos) return "0";
+    return s.substr(p);
 }
 
-string BigInt::subtract(string num1, string num2) const {
-    string result = "";
-    int n1 = num1.length(), n2 = num2.length();
-    reverse(num1.begin(), num1.end());
-    reverse(num2.begin(), num2.end());
-    int carry = 0;
-
-    for (int i = 0; i < n2; i++) {
-        int sub = ((num1[i] - '0') - (num2[i] - '0') - carry);
-        if (sub < 0) {
-            sub = sub + 10;
-            carry = 1;
-        } else {
-            carry = 0;
-        }
-        result.push_back(sub + '0');
+string BigInt::add(const string& num1, const string& num2) const {
+    string a = num1; string b = num2;
+    reverse(a.begin(), a.end()); reverse(b.begin(), b.end());
+    string res; int carry = 0;
+    int n = max(a.size(), b.size());
+    for (int i = 0; i < n; ++i) {
+        int da = (i < (int)a.size()) ? a[i]-'0' : 0;
+        int db = (i < (int)b.size()) ? b[i]-'0' : 0;
+        int s = da + db + carry;
+        res.push_back(char('0' + (s % 10)));
+        carry = s / 10;
     }
-
-    for (int i = n2; i < n1; i++) {
-        int sub = ((num1[i] - '0') - carry);
-        if (sub < 0) {
-            sub = sub + 10;
-            carry = 1;
-        } else {
-            carry = 0;
-        }
-        result.push_back(sub + '0');
-    }
-
-    reverse(result.begin(), result.end());
-    // Remove leading zeros
-    result.erase(0, min(result.find_first_not_of('0'), result.size()-1));
-    return result;
+    if (carry) res.push_back(char('0' + carry));
+    reverse(res.begin(), res.end());
+    return strip_leading_zeros(res);
 }
 
-string BigInt::multiply(string num1, string num2) const {
+string BigInt::subtract(const string& num1, const string& num2) const {
+    // assumes num1 >= num2 (both positive strings)
+    string a = num1; string b = num2;
+    reverse(a.begin(), a.end()); reverse(b.begin(), b.end());
+    string res; int carry = 0;
+    for (int i = 0; i < (int)a.size(); ++i) {
+        int da = a[i]-'0';
+        int db = (i < (int)b.size()) ? b[i]-'0' : 0;
+        int s = da - db - carry;
+        if (s < 0) { s += 10; carry = 1; } else carry = 0;
+        res.push_back(char('0' + s));
+    }
+    while (res.size() > 1 && res.back() == '0') res.pop_back();
+    reverse(res.begin(), res.end());
+    return strip_leading_zeros(res);
+}
+
+string BigInt::multiply(const string& num1, const string& num2) const {
     if (num1 == "0" || num2 == "0") return "0";
-
-    vector<int> res(num1.length() + num2.length(), 0);
-    int i_n1 = 0;
-    int i_n2 = 0;
-
-    for (int i = num1.length() - 1; i >= 0; i--) {
-        int carry = 0;
-        int n1 = num1[i] - '0';
-        i_n2 = 0;
-
-        for (int j = num2.length() - 1; j >= 0; j--) {
-            int n2 = num2[j] - '0';
-            int sum = n1 * n2 + res[i_n1 + i_n2] + carry;
-            carry = sum / 10;
-            res[i_n1 + i_n2] = sum % 10;
-            i_n2++;
+    int n1 = num1.size(), n2 = num2.size();
+    vector<int> res(n1 + n2, 0);
+    for (int i = n1-1; i >= 0; --i) {
+        for (int j = n2-1; j >= 0; --j) {
+            int d1 = num1[i]-'0';
+            int d2 = num2[j]-'0';
+            int sum = res[i+j+1] + d1 * d2;
+            res[i+j+1] = sum % 10;
+            res[i+j] += sum / 10;
         }
-
-        if (carry > 0) {
-            res[i_n1 + i_n2] += carry;
-        }
-        i_n1++;
     }
-
-    int i = res.size() - 1;
-    while (i >= 0 && res[i] == 0) i--;
-
-    if (i == -1) return "0";
-
-    string s = "";
-    while (i >= 0) s += to_string(res[i--]);
-
-    return s;
+    string s;
+    for (int x : res) if (!(s.empty() && x == 0)) s.push_back('0' + x);
+    return s.empty() ? "0" : s;
 }
 
-// --- Operator Overloading Implementations ---
+// operators
 BigInt BigInt::operator+(const BigInt& other) const {
-    BigInt result;
+    BigInt out;
     if (is_negative == other.is_negative) {
-        result.number = add(number, other.number);
-        result.is_negative = is_negative;
+        out.number = add(number, other.number);
+        out.is_negative = is_negative;
     } else {
-        // Different signs, perform subtraction
-        if (number.length() > other.number.length() || (number.length() == other.number.length() && number > other.number)) {
-            result.number = subtract(number, other.number);
-            result.is_negative = is_negative;
+        // different signs -> subtraction
+        if (number.size() > other.number.size() || (number.size() == other.number.size() && number >= other.number)) {
+            out.number = subtract(number, other.number);
+            out.is_negative = is_negative;
         } else {
-            result.number = subtract(other.number, number);
-            result.is_negative = other.is_negative;
+            out.number = subtract(other.number, number);
+            out.is_negative = other.is_negative;
         }
     }
-    return result;
+    if (out.number == "0") out.is_negative = false;
+    return out;
 }
 
 BigInt BigInt::operator-(const BigInt& other) const {
-    BigInt temp = other;
-    temp.is_negative = !temp.is_negative;
-    return *this + temp;
+    BigInt t = other; t.is_negative = !t.is_negative;
+    return *this + t;
 }
 
 BigInt BigInt::operator*(const BigInt& other) const {
-    BigInt result;
-    result.number = multiply(number, other.number);
-    result.is_negative = is_negative != other.is_negative;
-    if(result.number == "0") result.is_negative = false; // Zero is not negative
-    return result;
+    BigInt out;
+    out.number = multiply(number, other.number);
+    out.is_negative = (out.number == "0") ? false : (is_negative != other.is_negative);
+    return out;
 }
 
-bool BigInt::operator==(const BigInt& other) const {
-    return (number == other.number && is_negative == other.is_negative);
-}
-
-bool BigInt::operator!=(const BigInt& other) const {
-    return !(*this == other);
-}
-
-bool BigInt::operator<(const BigInt& other) const {
-    if (is_negative != other.is_negative) return is_negative;
-    if (is_negative) { // Both are negative
-        if (number.length() != other.number.length()) return number.length() > other.number.length();
-        return number > other.number;
-    } else { // Both are positive
-        if (number.length() != other.number.length()) return number.length() < other.number.length();
-        return number < other.number;
+// comparisons
+bool BigInt::operator==(const BigInt& o) const { return number == o.number && is_negative == o.is_negative; }
+bool BigInt::operator!=(const BigInt& o) const { return !(*this == o); }
+bool BigInt::operator<(const BigInt& o) const {
+    if (is_negative != o.is_negative) return is_negative;
+    if (!is_negative) {
+        if (number.size() != o.number.size()) return number.size() < o.number.size();
+        return number < o.number;
+    } else {
+        if (number.size() != o.number.size()) return number.size() > o.number.size();
+        return number > o.number;
     }
 }
-
-bool BigInt::operator>(const BigInt& other) const {
-    return other < *this;
-}
-
-bool BigInt::operator<=(const BigInt& other) const {
-    return !(*this > other);
-}
-
-bool BigInt::operator>=(const BigInt& other) const {
-    return !(*this < other);
-}
+bool BigInt::operator>(const BigInt& o) const { return o < *this; }
+bool BigInt::operator<=(const BigInt& o) const { return !(*this > o); }
+bool BigInt::operator>=(const BigInt& o) const { return !(*this < o); }
 
 ostream& operator<<(ostream& os, const BigInt& bi) {
-    if (bi.is_negative) {
-        os << "-";
-    }
+    if (bi.is_negative && bi.number != "0") os << "-";
     os << bi.number;
     return os;
 }
